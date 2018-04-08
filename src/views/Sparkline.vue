5f01748042f3fad50fa79a01b61a835ef9771b33
zhen.tang <zhen.tang@zhaopin.com>
2018-04-08 19:34:49 +0800

    Added bar chart example.

--- src/views/Sparkline.vue
+++ src/views/Sparkline.vue
@@ -4,14 +4,14 @@
     <p>一类信息体积小和数据密度高的图表。（更多功能待完善）</p>
     <h3>折线图</h3>
     <div class="demo">
-      <sparkline :data="spData1" :limit="spData1.length" width="200" height="50" :children="['line']" :lineStyles="spLineStyles1" :spotStyles="spSpotStyles1" :refLineType="spRefLineType1" />
+      <sparkline :data="spData1" :limit="spData1.length" width="200" height="50" :chartType="['line']" :lineStyles="spLineStyles1" :spotStyles="spSpotStyles1" :refLineType="spRefLineType1" />
       <div class="figure">
         <pre v-highlight><code class="html">&lt;sparkline
   :data="spData1"
   :limit="spData1.length"
   width="200"
   height="50"
-  :children="['line']"
+  :chartType="['line']"
   :lineStyles="spLineStyles1"
   :spotStyles="spSpotStyles1"
   :refLineType="spRefLineType1" /&gt;</code></pre>
@@ -45,14 +45,14 @@
     </div>
     <h3>曲线图</h3>
     <div class="demo">
-      <sparkline :data="spData2" :limit="spData2.length" width="200" height="50" :children="['curve']" :curveStyles="spCurveStyles2" :spotStyles="spSpotStyles2" :refLineType="spRefLineType2" />
+      <sparkline :data="spData2" :limit="spData2.length" width="200" height="50" :chartType="['curve']" :curveStyles="spCurveStyles2" :spotStyles="spSpotStyles2" :refLineType="spRefLineType2" />
       <div class="figure">
         <pre v-highlight><code class="html">&lt;sparkline
   :data="spData2"
   :limit="spData2.length"
   width="200"
   height="50"
-  :children="['curve']"
+  :chartType="['curve']"
   :curveStyles="spCurveStyles2"
   :spotStyles="spSpotStyles2"
   :refLineType="spRefLineType2" /&gt;</code></pre>
@@ -86,7 +86,7 @@
     </div>
     <h3>定制图表</h3>
     <div class="demo sparkline-with-text">
-      <sparkline :data="spData3" :limit="spData3.length" :spotlight="spIndex3" width="200" height="50" :children="['curve']" :curveStyles="spCurveStyles3" :spotStyles="spSpotStyles3" :refLineStyles="spRefLineStyles3" :text="`${spData3[spIndex3]}`" :textStyles="spTextStyles3" />
+      <sparkline :data="spData3" :limit="spData3.length" :spotlight="spIndex3" width="200" height="50" :chartType="['curve']" :curveStyles="spCurveStyles3" :spotStyles="spSpotStyles3" :refLineStyles="spRefLineStyles3" :text="`${spData3[spIndex3]}`" :textStyles="spTextStyles3" :mouseEvents="spMouseEvents3" />
       <div class="figure">
         <pre v-highlight><code class="html">&lt;sparkline
   :data="spData3"
@@ -94,7 +94,7 @@
   :spotlight="spIndex3"
   width="200"
   height="50"
-  :children="['curve']"
+  :chartType="['curve']"
   :curveStyles="spCurveStyles3"
   :spotStyles="spSpotStyles3"
   :refLineStyles="spRefLineStyles3"
@@ -125,7 +125,7 @@
       spRefLineStyles3: {
         stroke: '#d14',
         strokeOpacity: 1,
-        strokeDasharray: '5, 5'
+        strokeDasharray: '2, 2'
       },
       // 当前结点
       spIndex3: 3,
@@ -133,6 +133,46 @@
       spTextStyles3: {
         color: '#d14',
         fontSize: '10px'
+      }
+    }
+  }
+}</code></pre>
+      </div>
+    </div>
+    <h3>柱状图</h3>
+    <div class="demo sparkline-with-text">
+      <sparkline :data="spData4" :limit="spData4.length" width="200" height="50" :chartType="['bar', 'line']" :lineStyles="spLineStyles4" :barStyles="spBarStyles4" :spotStyles="spSpotStyles4" :mouseEvents="spMouseEvents4" />
+      <div class="figure">
+        <pre v-highlight><code class="html">&lt;sparkline
+  :data="spData4"
+  :limit="spData4.length"
+  width="200"
+  height="50"
+  :chartType="['bar', 'line']"
+  :lineStyles="spLineStyles4"
+  :barStyles="spBarStyles4"
+  :spotStyles="spSpotStyles4" /&gt;</code></pre>
+      </div>
+      <div class="figure">
+        <pre v-highlight><code class="javascript">export default {
+  data () {
+    return {
+      // 数据集合
+      spData4: (() => {
+        const len = 10
+        return Array.from({
+          length: len
+        }, () => Math.floor(Math.random() * len))
+      })(),
+      spBarStyles4: {
+        fill: '#54a5ff'
+      },
+      spLineStyles4: {
+        stroke: '#d14'
+      },
+      spSpotStyles4: {
+        strokeOpacity: 0,
+        fillOpacity: 0
       }
     }
   }
@@ -190,12 +230,36 @@
         spRefLineStyles3: {
           stroke: '#d14',
           strokeOpacity: 1,
-          strokeDasharray: '5, 5'
+          strokeDasharray: '2, 2'
         },
         spIndex3: 3,
         spTextStyles3: {
           color: '#d14',
           fontSize: '10px'
+        },
+        spMouseEvents3 (evt, data, pos) {
+          if (evt === 'mousedown') {
+            console.log(data, pos)
+          }
+        },
+        spData4: (() => {
+          const len = 10
+          return Array.from({
+            length: len
+          }, () => Math.floor(Math.random() * len))
+        })(),
+        spBarStyles4: {
+          fill: '#54a5ff'
+        },
+        spLineStyles4: {
+          stroke: '#d14'
+        },
+        spSpotStyles4: {
+          strokeOpacity: 0,
+          fillOpacity: 0
+        },
+        spMouseEvents4 (evt, data, pos) {
+
         }
       }
     }
